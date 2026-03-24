723a89063f22e02009c1d8a193e845c9f6b374bd
rniheeth44-eng <rniheeth44@gmail.com>
2026-03-25 01:56:32 +0530

    Add src/handlers/panelHandler.js

---
+++ src/handlers/panelHandler.js
@@ -0,0 +1,34 @@
+const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
+
+async function spawnPanel(channel, guild) {
+  const embed = new EmbedBuilder()
+    .setColor(0x00c853)
+    .setTitle('Cryptocurrency')
+    .setDescription(
+      '**Fees:**\n' +
+      '• Deals $250+: 1%\n' +
+      '• Deals under $250: $2\n' +
+      '• Deals under $50: $0.50\n' +
+      '• __Deals under $10 are **FREE**__\n' +
+      '• USDT & USDC have $1 subcharge\n\n' +
+      'Press the dropdown below to select & initiate a deal with supported cryptocurrencies.'
+    );
+
+  const select = new StringSelectMenuBuilder()
+    .setCustomId('crypto_select')
+    .setPlaceholder('Make a selection')
+    .addOptions([
+      new StringSelectMenuOptionBuilder().setLabel('BTC').setValue('BTC').setEmoji('🟠'),
+      new StringSelectMenuOptionBuilder().setLabel('ETH').setValue('ETH').setEmoji('🔷'),
+      new StringSelectMenuOptionBuilder().setLabel('LTC').setValue('LTC').setEmoji('⬜'),
+      new StringSelectMenuOptionBuilder().setLabel('SOL').setValue('SOL').setEmoji('🟣'),
+      new StringSelectMenuOptionBuilder().setLabel('USDT [ERC-20]').setValue('USDT [ERC-20]').setEmoji('💚'),
+      new StringSelectMenuOptionBuilder().setLabel('USDC [ERC-20]').setValue('USDC [ERC-20]').setEmoji('🔵'),
+    ]);
+
+  const row = new ActionRowBuilder().addComponents(select);
+
+  await channel.send({ embeds: [embed], components: [row] });
+}
+
+module.exports = { spawnPanel };
