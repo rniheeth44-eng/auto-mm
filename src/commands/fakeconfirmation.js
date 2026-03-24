a73e99dd74e7ada1479d9cd1619770bc98cd4165
rniheeth44-eng <rniheeth44@gmail.com>
2026-03-25 01:56:32 +0530

    Add src/commands/fakeconfirmation.js

---
+++ src/commands/fakeconfirmation.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('fakeconfirmation')
+    .setDescription('Simulate a transaction detected notification')
+    .addNumberOption(option =>
+      option.setName('money')
+        .setDescription('Amount in USD received')
+        .setRequired(true)
+    ),
+
+  async execute(interaction, client) {
+    const usdAmount = interaction.options.getNumber('money');
+
+    // Get LTC price from API or use fallback
+    let ltcPrice = 85;
+    try {
+      const axios = require('axios');
+      const resp = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd', { timeout: 5000 });
+      ltcPrice = resp.data?.litecoin?.usd || 85;
+    } catch (e) {
+      // fallback
+    }
+
+    const ltcAmount = (usdAmount / ltcPrice).toFixed(8);
+
+    const embed = new EmbedBuilder()
+      .setColor(0x00c853)
+      .setTitle('Transaction has been detected')
+      .setDescription('Wait for the transaction to receive the required number of confirmations.')
+      .addFields(
+        { name: 'Transaction', value: '[View Transaction](https://blockchair.com/litecoin)', inline: false },
+        { name: 'Required Confirmations', value: '1', inline: false },
+        { name: 'Amount Received', value: `${ltcAmount} LTC ($${usdAmount.toFixed(2)} USD)`, inline: false }
+      )
+      .setThumbnail('https://i.imgur.com/4M34hi2.png');
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
