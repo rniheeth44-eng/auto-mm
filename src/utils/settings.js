161419b6a6325a18dcefbd9a96490efa0274913f
rniheeth44-eng <rniheeth44@gmail.com>
2026-03-26 00:30:36 +0530

    update src/utils/settings.js

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -5,7 +5,12 @@ const SETTINGS_PATH = path.join(__dirname, '../../settings.json');
 
 const settings = {
   ltcAddress: 'ltc1qzqt2gudkap6js5a3puqymyx9vydrnfchnupdup',
+  btcAddress: null,
+  ethAddress: null,
+  solAddress: null,
+  usdtAddress: null,
   logChannelId: null,
+  mercyRoleId: null,
 };
 
 try {
@@ -19,7 +24,25 @@ function save() {
 
 function getLtcAddress() { return settings.ltcAddress; }
 function setLtcAddress(addr) { settings.ltcAddress = addr; save(); }
+function getBtcAddress() { return settings.btcAddress; }
+function setBtcAddress(addr) { settings.btcAddress = addr; save(); }
+function getEthAddress() { return settings.ethAddress; }
+function setEthAddress(addr) { settings.ethAddress = addr; save(); }
+function getSolAddress() { return settings.solAddress; }
+function setSolAddress(addr) { settings.solAddress = addr; save(); }
+function getUsdtAddress() { return settings.usdtAddress; }
+function setUsdtAddress(addr) { settings.usdtAddress = addr; save(); }
 function getLogChannelId() { return settings.logChannelId; }
 function setLogChannelId(id) { settings.logChannelId = id; save(); }
+function getMercyRoleId() { return settings.mercyRoleId; }
+function setMercyRoleId(id) { settings.mercyRoleId = id; save(); }
 
-module.exports = { getLtcAddress, setLtcAddress, getLogChannelId, setLogChannelId };
+module.exports = {
+  getLtcAddress, setLtcAddress,
+  getBtcAddress, setBtcAddress,
+  getEthAddress, setEthAddress,
+  getSolAddress, setSolAddress,
+  getUsdtAddress, setUsdtAddress,
+  getLogChannelId, setLogChannelId,
+  getMercyRoleId, setMercyRoleId,
+};
