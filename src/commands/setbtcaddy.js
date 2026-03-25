4dc14748e37b5b416ed5e745d2ef5ed2e14f0b4c
rniheeth44-eng <rniheeth44@gmail.com>
2026-03-26 00:31:05 +0530

    add src/commands/setbtcaddy.js

---
+++ src/commands/setbtcaddy.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { setBtcAddress } = require('../utils/settings');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('setbtcaddy')
+    .setDescription('Set the BTC wallet address shown in payment invoices')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addStringOption(option =>
+      option.setName('address')
+        .setDescription('The BTC wallet address')
+        .setRequired(true)
+    ),
+
+  async execute(interaction, client) {
+    await interaction.deferReply({ flags: 64 });
+    const address = interaction.options.getString('address');
+    setBtcAddress(address);
+    await interaction.editReply({ content: `BTC address updated to:\n\`${address}\`` });
+  }
+};
