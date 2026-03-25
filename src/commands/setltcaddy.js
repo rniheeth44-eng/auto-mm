7bdfc0c85ac7f7e1be1814f47db78db559c2b32c
rniheeth44-eng <rniheeth44@gmail.com>
2026-03-26 00:31:08 +0530

    add src/commands/setltcaddy.js

---
+++ src/commands/setltcaddy.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { setLtcAddress } = require('../utils/settings');
+const { startMonitor } = require('../utils/monitor');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('setltcaddy')
+    .setDescription('Set the LTC wallet address shown in payment invoices')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addStringOption(option =>
+      option.setName('address')
+        .setDescription('The LTC wallet address')
+        .setRequired(true)
+    ),
+
+  async execute(interaction, client) {
+    await interaction.deferReply({ flags: 64 });
+    const address = interaction.options.getString('address');
+    setLtcAddress(address);
+    startMonitor(address, client);
+    await interaction.editReply({ content: `LTC address updated to:\n\`${address}\`\n\nBlockchain monitor restarted.` });
+  }
+};
